6b8034db6a30e296a4ff73fa069fb67471fedf8b
linx02 <elvius.linus@gmail.com>
2023-10-09 15:04:48 +0200

    Build base for project summary page

---
+++ pages/1_Project_summary.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('Project Summary')
+
+st.write('General info')
+st.write('* lorem ipsum')
+
+st.write('Project Dataset')
+st.write('* The available dataset contains 3000 out of 200k+ images taken from the ... dataset')
+
+st.write('Link to additional info (README.md)')
+
+st.write('Business requirements')
+st.write('* The client is interested in a machine learning solution to predict the gender of a person based on a picture of their face')
+st.write('* The client is interested in a study of the data presented visually in order to understand the data better')
