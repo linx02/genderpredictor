8bd947e67a26bd2828fb2ac590091a3fb5473ae7
linx02 E <elvius.linus@gmail.com>
2023-10-10 08:16:36 +0000

    Create hypothesis & validation page

--- pages/4_Hypothesis_and_validation.py
+++ pages/4_Hypothesis_and_validation.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title('Hypothesis and Validation')
+
+st.info('We suspect that men and women have gender-specific facial features that differentiate them.')
+st.write('* An average image study can help to investigate this hypothesis')
+
+st.success('This hypothesis was validated by:')
+st.write('* Calculating 2 averages images from the available dataset. One image for the male gender and one for the female gender.')
+st.write('* Calculating an image for the difference between the averages by subtracting one of the images from the other in order to visually see the exact difference.')
