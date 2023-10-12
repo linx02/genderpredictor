c3f435ee7abc78f3392141635c300ff14824b45c
linx02 <elvius.linus@gmail.com>
2023-10-12 16:54:07 +0200

    Switch to tensorflow-cpu

--- pages/3_Predict_gender.py
+++ pages/3_Predict_gender.py
@@ -1,6 +1,6 @@
 # Import modules
 import streamlit as st
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
 import pandas as pd
