6644e4155ac831463892b7a9951333a7ba849ac5
linx02 <elvius.linus@gmail.com>
2023-10-13 13:49:41 +0200

    Create api for prediction on model

--- pages/3_Predict_gender.py
+++ pages/3_Predict_gender.py
@@ -1,17 +1,14 @@
 # Import modules
 import streamlit as st
-from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
 import pandas as pd
 import os
+import requests
 
 # Vars
 ROOT_DIR = os.getcwd()
 LIVE_PREDICTION_DIR = os.path.join(ROOT_DIR, 'assets', 'live_prediction_images.zip')
-
-# Load model
-model = load_model('model.h5')
 
 # Page title
 st.title('Predict Gender from image')
@@ -32,35 +29,21 @@ if st.button("Predict"):
     if len(uploaded_files) > 0:
         predictions = {}
         for image in uploaded_files:
-            img_pil = Image.open(image)  # Open image using pillow
-            # Process image for prediction
-            resized_img = img_pil.resize((200, 200))  # Resize image
-            img_array = np.array(resized_img)  # Convert to numpy array
-            img_array = img_array / 255  # Normalize image
-            img_array = np.expand_dims(img_array, axis=0)  # Reshape to (Sample size, 200, 200, 3)
-            prediction = model.predict(img_array)  # Make prediction
+            # Send image to API
+            response = requests.post('http://127.0.0.1:5000/predict', files={'file': image})
+            # Save prediction
+            predictions[image.name] = response.text
+            st.write('Prediction: ', response.text)
 
-            # If prediction = [smaller, bigger] (Male)
-            if prediction[0][1] > prediction[0][0]:
-                st.write('Predicted class: **Male**')
-                predictions[image.name] = 'male'
-            # If prediction = [bigger, smaller] (Female)
-            elif prediction[0][1] < prediction[0][0]:
-                st.write('Predicted class: **Female**')
-                predictions[image.name] = 'female'
-            # In the extremely rare case that predictions = [0.50, 0.50]
-            elif prediction[0][1] == prediction[0][0]:
-                st.write('Predicted class: **Unknown**')
-                predictions[image.name] = 'unknown'
+            # Display image
+            image = Image.open(image)
+            st.image(image, caption='Uploaded Image.', use_column_width=False)
 
-            # Show image
-            st.image(img_pil)
-
-            df_report = pd.DataFrame(predictions.items(), columns=['filename', 'prediction'])
-            st.success('Analysis report')
-            st.table(df_report)
-            csv_data = df_report.to_csv(index=False)
-            st.download_button("Download Report", csv_data, "report.csv", key='csv_download_button')
+        df_report = pd.DataFrame(predictions.items(), columns=['filename', 'prediction'])
+        st.success('Analysis report')
+        st.table(df_report)
+        csv_data = df_report.to_csv(index=False)
+        st.download_button("Download Report", csv_data, "report.csv", key='csv_download_button')
             
     else:
         st.error('No files uploaded')
