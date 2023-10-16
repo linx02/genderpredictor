d0521f0ca45c900a3d218d75f2aee5c10fa4dd14
linx02 <elvius.linus@gmail.com>
2023-10-16 10:45:04 +0200

    Update project summary page

--- pages/1_Project_summary.py
+++ pages/1_Project_summary.py
@@ -4,14 +4,26 @@ import streamlit as st
 # Page title and text
 st.title('Project Summary')
 
-st.write('General info')
-st.write('* lorem ipsum')
+st.info('''
+__General info__
+* In order to respect the privacy of users, as well as to apply to a broader group of people, businesses often allow users to register to their sites with gender information outside of the male/female binary.
 
-st.write('Project Dataset')
-st.markdown('* The available dataset contains 3000 out of 200k+ images taken from the [Gender Classification 203K Images | CelebA](https://www.kaggle.com/datasets/ashishjangra27/gender-recognition-200k-images-celeba) dataset which is licensed under [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/).')
+_Gender data is commonly used in the following ways:_
+1. Analysis of user data in order to understand the userbase better.
+2. Personalization of user experience in order to provide a more tailored experience to the user.
+3. Personalization of advertisements in order to increase the effectiveness of the advertisement.''')
 
-st.markdown('Additional information can be found on the projects README.md file which can be found [here](https://github.com/linx02/genderpredictor#readme)')
+st.success('''
+__Project Dataset__
+* The available dataset is sourced from [Kaggle](https://www.kaggle.com/datasets) and contains 3000 out of 200k+ images taken from the [Gender Classification 203K Images | CelebA](https://www.kaggle.com/datasets/ashishjangra27/gender-recognition-200k-images-celeba) dataset which is licensed under [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/).
+* The original source of this dataset is the [CelebA dataset](http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html)
+''')
 
-st.write('Business requirements')
-st.write('* The client is interested in a machine learning solution to predict the gender of a person based on a picture of their face')
-st.write('* The client is interested in a study of the data presented visually in order to understand the data better')
+st.markdown('__Additional information can be found on the projects README.md file which can be found [here](https://github.com/linx02/genderpredictor#readme)__')
+
+st.warning('''
+__Business requirements__
+1. The client is interested in a machine learning solution to predict the gender of a person based on a picture of their face
+2. The client is interested in a study of the data presented visually in order to understand the data better
+3. The client is interested in an API in order to integrate the solution into their own applications
+''')
---
+++ pages/6_API.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# Page title and text
+st.title('API')
+s
+st.markdown('Link to the GitHub repository for the API can be found [here](https://github.com/linx02/genderpredictor-api)')
